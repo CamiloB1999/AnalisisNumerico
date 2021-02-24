610262c79dd7c84470d868d53ba61f4c4015760a
Juan Carlos Suarez <suarezmjuan@gmail.com>
2021-02-23 21:23:28 -0500

    Se agrega código para graficación de errores punto fijo

--- RaicesFeb2021/punto_fijo.py
+++ RaicesFeb2021/punto_fijo.py
@@ -25,20 +25,31 @@ from decimal import *
 import math
 from scipy.special import lambertw
 
-def puntoFijo(x, error_permitido, g, iters):
+def puntoFijo(x, error_permitido, g, iters, num_eq):
     iteraciones = 0
     y_vals = []
     y_vals.append(x)
+    errores = []
     while abs(g(x)-x) > error_permitido and iteraciones < iters:
+        error = abs(g(x)-x)
+        errores.append(error)
         iteraciones += 1
         x = g(x)
         y_vals.append(x)
+    
+    output_file(f"errores_puntoFijo{num_eq}- {error_permitido}.html")
+    fig = figure()
+    x_errores = list(range(len(y_vals)))
+    fig.line(x_errores, errores, line_width=2)
+
     if x > 0.7 and x < 0.75:
         print(f'Se obtuvieron las raices {Decimal(x)} y {Decimal(x * -1)} en {iteraciones} iteraciones')
+        show(fig)
     elif x > 14 and x < 15: 
         print(f'Se obtuvo la raiz {x} en {iteraciones} iteraciones')
     else: 
         print(f'Se obtuvo la raiz {Decimal(x)} en {iteraciones} iteraciones')
+        show(fig)
     return y_vals
 
 if __name__ == "__main__": 
@@ -49,38 +60,38 @@ if __name__ == "__main__":
     #Ecuación 1
     g = lambda x: np.cos(x)
     for tol in tols: 
-        y_vals = puntoFijo(x,tol, g, 1000)
+        y_vals = puntoFijo(x,tol, g, 1000, 1)
         output_file(f"punto_fijo1-{tol}.html")
         fig = figure()
         x_vals = list(range(len(y_vals)))
         fig.line(x_vals, y_vals, line_width=2)
-        show(fig)
+        #show(fig)
 
     #Ecuación 2
     g = lambda x: 1/(np.sin(x))
     for tol in tols: 
-        y_vals = puntoFijo(x,tol, g, 1000)
+        y_vals = puntoFijo(x,tol, g, 1000, 2)
         output_file(f"punto_fijo2-{tol}.html")
         fig = figure()
         x_vals = list(range(len(y_vals)))
         fig.line(x_vals, y_vals, line_width=2)
-        show(fig)
+        #show(fig)
 
     #Ecuación 3
     g = lambda x: ((2*x**2) - (4/3*x) + (8/27))**(1/3)
     for tol in tols: 
-        y_vals = puntoFijo(x,tol, g, 1000)
+        y_vals = puntoFijo(x,tol, g, 1000, 3)
         output_file(f"punto_fijo3-{tol}.html")
         fig = figure()
         x_vals = list(range(len(y_vals)))
         fig.line(x_vals, y_vals, line_width=2)
-        show(fig)
+        #show(fig)
 
     #Ecuación 4
     e = math.e
     g = lambda x: 681 * (20* lambertw(-49/(20*e**(49/20)))+49)/2000
     for tol in tols: 
-        y_vals = puntoFijo(x,tol, g, 1000)
+        y_vals = puntoFijo(x,tol, g, 1000, 4)
         output_file(f"punto_fijo3-{tol}.html")
         fig = figure()
         x_vals = list(range(len(y_vals)))
@@ -90,9 +101,9 @@ if __name__ == "__main__":
     #Ecuación 5
     g = lambda x: (2*x + 5)**(1/3)
     for tol in tols: 
-        y_vals = puntoFijo(x,tol, g, 1000)
+        y_vals = puntoFijo(x,tol, g, 1000, 5)
         output_file(f"punto_fijo5-{tol}.html")
         fig = figure()
         x_vals = list(range(len(y_vals)))
         fig.line(x_vals, y_vals, line_width=2)
-        show(fig)
+        #show(fig)
